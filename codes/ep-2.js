9c82558796e57bd581816fe389717ffaa8dea345
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-09-05 20:07:43 +0530

    episode-2 code

---
+++ codes/ep-2.js
@@ -0,0 +1,14 @@
+var n = 2;  // in first phase memory allocated for variables and function and the value of variabel is undefined and the value of fuction is hold the code inside the function 
+function square(num) {
+  var ans = num * num;
+  return ans;
+
+}
+var square2 = square(n);
+var square4 = square(4);
+// in code execution phase  the variable value is assigne to that particular variable and fuction values 
+
+// after all execution of code the  functions are poped out from call stack .
+
+
+
