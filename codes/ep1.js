081bdb3eef0d83c9c8e0477edd0e6d6f9be5dceb
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-09-05 16:37:32 +0530

    episode-1 code

---
+++ codes/ep1.js
@@ -0,0 +1,8 @@
+// key value pairs
+  
+let x=50  // in this code x is key and 50 is value 
+
+function demo()   //in this fuction demo is key and the  value is code inside the fuction
+{
+     //code 
+}
