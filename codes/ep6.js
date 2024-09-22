d9235934635e54ea8a269701fc790a62926baf17
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-09-22 12:49:40 +0530

    episode-6

---
+++ codes/ep6.js
@@ -0,0 +1,5 @@
+console.log(x); // undefined (x is declared but no value yet)
+var x = 25;
+console.log(x); // 25 (x now has a value)
+console.log(a); // Uncaught ReferenceError: a is not defined (a is not declared)
+
