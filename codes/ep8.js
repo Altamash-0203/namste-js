0b37b35453f181c150e6fd3114ec633f857924f3
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-10-09 16:41:30 +0530

    let & const in JS, Temporal Dead Zone

--- codes/ep8.js
+++ codes/ep8.js
@@ -22,6 +22,7 @@
     const b = 100;
     b = 1000; // TypeError: Assignment to constant variable.
 
+/* ----------------------------------------------------------------*/
 
 
 //temproral dead zone (TDZ)
@@ -33,5 +34,27 @@ console.log(a);
 
 let a = 10;
 
-//.
+/* ----------------------------------------------------------------*/
+
+
+//types of errors
+
+
+// 1- ReferenceError  
+
+    console.log(err)  // if we access value before intilization then it shows reffrence error
+
+
+//2-syntx error
+
+   let a=10
+   let a=100   // if we redecaler and let variable then it throws syntax error
+
+//Type error
+
+ const b=10
+  b=100        //if we change const varible then it throws type error
+
+
+
 
