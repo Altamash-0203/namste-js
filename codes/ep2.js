7631c8135fc3a01e9596f13a5db7aab287bc0d64
Altamsh <mr.shaikh.altamash0203@gmail.com>
2024-12-19 16:31:15 +0530

    how js program excute behind

--- codes/ep2.js
+++ codes/ep2.js
@@ -1,13 +1,14 @@
-var n = 2;  // in first phase memory allocated for variables and function and the value of variabel is undefined and the value of fuction is hold the code inside the function 
-function square(num) {
-  var ans = num * num;
-  return ans;
 
+
+var n=2; 
+
+function square (num)
+{
+  var ans=num*num
+  return ans;  // retuns the control where function in invoked eg:line 10 &11
 }
-var square2 = square(n);
-var square4 = square(4);
-// in code execution phase  the variable value is assigne to that particular variable and fuction values 
+var square2=square(n) //the value of n=2 is now argument of function
+var square4=square(4)
 
-// after all execution of code the  functions are poped out from call stack.
-
-
+console.log(square2);
+console.log(square4);
